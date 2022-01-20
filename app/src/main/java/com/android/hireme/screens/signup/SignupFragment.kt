c1b1bdedd15c39d9224f3c56e91d94b5f525435e
shubhamdwd <shubhamdwd3@gmail.com>
2022-01-20 16:36:31 +0530

    SignUp Process

--- app/src/main/java/com/android/hireme/screens/signup/SignupFragment.kt
+++ app/src/main/java/com/android/hireme/screens/signup/SignupFragment.kt
@@ -1,7 +1,10 @@
 package com.android.hireme.screens.signup
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +13,33 @@ import android.widget.Toast
 import android.widget.AdapterView.OnItemClickListener
 
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.android.hireme.activity.HireMeActivity
+import com.android.hireme.base.BaseFragment
+import com.android.hireme.databinding.FragmentSignupBinding
+import com.android.hireme.factory.ViewModelsFactory
+import com.android.hireme.util.Status
+import com.android.hireme.util.TOAST_ERROR_TYPE
+import com.android.hireme.util.TOAST_SUCCESS_TYPE
+import com.android.hireme.util.showCustomToast
+import com.android.hireme.viewmodels.SignUpViewModel
 
 
-class SignupFragment : Fragment() {
+class SignupFragment : BaseFragment(){
+    lateinit var navController: NavController
+    lateinit var mBinding: FragmentSignupBinding
+    lateinit var signUpViewModel : SignUpViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        context?.let { setProgressBar(it) }
     }
+
     val list = arrayOf(
         "Cristiano Ronaldo",
         "Messi",
@@ -29,38 +51,104 @@ class SignupFragment : Fragment() {
         "Ziyech",
         "Suarez"
     )
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        signUpViewModel = ViewModelProviders.of(
+            this,
+            ViewModelsFactory()
+        ).get(SignUpViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_signup, container, false)
-        // Inflate the layout for this fragment
-        val selectRole = view.findViewById<AutoCompleteTextView>(R.id.selectRole)
+
+        mBinding  = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_signup, container, false)
+        mBinding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = signUpViewModel
+        }
+
+        return mBinding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
         val arrayAdapter = context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, it.resources.getStringArray(R.array.select_roles)) }
 //            ArrayAdapter(
 //            this,
 //            android.R.layout.simple_list_item_1, list
 //        )
         var selection: String
-        selectRole.setAdapter(arrayAdapter)
-        selectRole.setThreshold(1)
-        selectRole.setCursorVisible(false)
-        selectRole.setOnClickListener {
-            selectRole.showDropDown()
+        mBinding.selectRole.setAdapter(arrayAdapter)
+        mBinding.selectRole.setThreshold(1)
+        mBinding.selectRole.setCursorVisible(false)
+        mBinding.selectRole.setOnClickListener {
+            mBinding.selectRole.showDropDown()
         }
-        selectRole.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
-            selectRole.showDropDown()
+        mBinding.selectRole.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
+            mBinding.selectRole.showDropDown()
             selection = parent.getItemAtPosition(position) as String
             Toast.makeText(
                 context, selection,
                 Toast.LENGTH_SHORT
             )
         })
-        return view
+
+        signUpViewModel.getSignUpResponse().observe(viewLifecycleOwner, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    dismissProgressBar()
+                    it.data?.let { it1 ->
+                        Toast(context).showCustomToast(
+                            TOAST_SUCCESS_TYPE, it1.message,
+                            context as Activity
+                        )
+
+                        val intent = Intent(context, HireMeActivity::class.java).apply {
+                            putExtra("loginResponse", it1)
+                        }
+                        startActivity(intent)
+                    }
+                }
+                Status.LOADING -> {
+                    showProgressBar()
+
+                }
+                Status.ERROR -> {
+                    dismissProgressBar()
+                    it.data?.let { it1 ->
+                        Toast(context).showCustomToast(
+                            TOAST_ERROR_TYPE, it1.message,
+                            context as Activity
+                        )
+                    }
+                }
+            }
+        })
+        mBinding.signUpButton.setOnClickListener {
+            if (signUpViewModel.isValidForm(mBinding.email.text.toString(), mBinding.password.text.toString(),mBinding.firstName.text.toString(), mBinding.lastName.text.toString(),mBinding.selectRole.text.toString())) {
+                signUpViewModel.signUp(mBinding.email.text.toString(), mBinding.password.text.toString(),mBinding.firstName.text.toString(), mBinding.lastName.text.toString(),mBinding.selectRole.text.toString())
+            }else{
+                Toast(context).showCustomToast(
+                    TOAST_ERROR_TYPE, "Please fill all the fields",
+                    context as Activity
+                )
+            }
+        }
     }
+
+
 
     companion object {
         @JvmStatic
         fun newInstance() = SignupFragment()
     }
+
+
 }
